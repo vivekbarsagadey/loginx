a45fb96f5abddb14c2aff5a613cf6d15a95deb1f
vivek.bnb <vivek.bnb@gmail.com>
2025-09-27 21:05:38 +0000

    fix issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,8 @@ import { Stack, useRouter, useSegments } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 import { AuthProvider, useAuth } from '@/hooks/use-auth-provider';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
