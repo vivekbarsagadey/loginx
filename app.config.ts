8c069da2c643aeb55b78c7408489310690b04c32
vivek.bnb <vivek.bnb@gmail.com>
2025-09-30 00:00:34 +0000

    working on env data

--- app.config.ts
+++ app.config.ts
@@ -2,15 +2,16 @@ import 'dotenv/config';
 
 export default {
   "expo": {
-    "name": "loginx",
+    "name": process.env.APP_NAME || "loginx",
     "slug": "loginx",
-    "version": "1.0.0",
+    "version": process.env.APP_VERSION || "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
     "scheme": "loginx",
     "userInterfaceStyle": "automatic",
     "ios": {
-      "supportsTablet": true
+      "supportsTablet": true,
+      "buildNumber": process.env.APP_BUILD || "100"
     },
     "android": {
       "adaptiveIcon": {
@@ -20,7 +21,8 @@ export default {
         "monochromeImage": "./assets/images/android-icon-monochrome.png"
       },
       "package": "com.whizit.loginx",
-      "minSdkVersion": 26
+      "minSdkVersion": 26,
+      "versionCode": parseInt(process.env.APP_BUILD || "100")
     },
     "web": {
       "output": "static",
@@ -50,12 +52,24 @@ export default {
       "eas": {
         "projectId": "524907f3-68cc-42d8-9b78-3561e304156d"
       },
-      "apiKey": process.env.API_KEY,
-      "authDomain": process.env.AUTH_DOMAIN,
-      "projectId": process.env.PROJECT_ID,
-      "storageBucket": process.env.STORAGE_BUCKET,
-      "messagingSenderId": process.env.MESSAGING_SENDER_ID,
-      "appId": process.env.APP_ID
+      "apiKey": process.env.API_KEY || "AIzaSyAGR1vYM4qWGQNok2grOaLyyvHYq8xcxc0",
+      "authDomain": process.env.AUTH_DOMAIN || "loginx-e289b.firebaseapp.com",
+      "projectId": process.env.PROJECT_ID || "loginx-e289b",
+      "storageBucket": process.env.STORAGE_BUCKET || "loginx-e289b.firebasestorage.app",
+      "messagingSenderId": process.env.MESSAGING_SENDER_ID || "26103201207",
+      "appId": process.env.APP_ID || "1:26103201207:web:d052568449b1bc63b53c32",
+      "appName": process.env.APP_NAME || "loginx",
+      "appVersion": process.env.APP_VERSION || "1.0.0",
+      "appBuild": process.env.APP_BUILD || "100",
+      "apiBaseUrl": process.env.API_BASE_URL || 'https://api.myapp.com',
+      "apiTimeout": process.env.API_TIMEOUT || '10000',
+      "wsUrl": process.env.WS_URL || 'wss://ws.myapp.com',
+      "dbName": process.env.DB_NAME || 'login-x-db',
+      "dbEncryptionKey": process.env.DB_ENCRYPTION_KEY || 'random-32byte-secret',
+      "cacheTtl": process.env.CACHE_TTL || '3600',
+      "sentryDsn": process.env.SENTRY_DSN || 'https://xxxx.ingest.sentry.io/1234',
+      "amplitudeKey": process.env.AMPLITUDE_KEY || 'abcd1234',
+      "googleMapsApiKey": process.env.GOOGLE_MAPS_API_KEY || 'yourkey'
     }
   }
-};
+}
--- env.d.ts
+++ env.d.ts
@@ -7,6 +7,18 @@ declare namespace NodeJS {
       MESSAGING_SENDER_ID: string;
       APP_ID: string;
       MEASUREMENT_ID: string;
+      APP_NAME: string;
+      APP_VERSION: string;
+      APP_BUILD: string;
+      API_BASE_URL: string;
+      API_TIMEOUT: string;
+      WS_URL: string;
+      DB_NAME: string;
+      DB_ENCRYPTION_KEY: string;
+      CACHE_TTL: string;
+      SENTRY_DSN: string;
+      AMPLITUDE_KEY: string;
+      GOOGLE_MAPS_API_KEY: string;
     }
   }
   
