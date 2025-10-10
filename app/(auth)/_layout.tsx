6a85defc2cd2d80d835fdb069cd9ceddf6b7b028
vivek <vivek.bnb@gmail.com>
2025-10-10 21:00:01 +0530

    refactor: Hide header in AuthLayout for a cleaner user interface

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout() {
   return (
     <Stack
       screenOptions={{
-        headerShown: true,
+        headerShown: false,
         headerStyle: {
           backgroundColor,
         },
